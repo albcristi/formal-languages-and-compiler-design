b4de3c3ff12cfad5c2f4e81b8e78f8b042a3b777
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-10-24 12:49:30 +0300

    done

---
+++ Lab3/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import model.MyScanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner scanner = new MyScanner("./source_programs/p1err.txt");
+        scanner.scan();
+    }
+}
